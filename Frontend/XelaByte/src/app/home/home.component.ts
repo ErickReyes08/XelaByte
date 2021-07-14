fb1b50d25e50e8ae0c859055488684c0668c05fa
AndyCRJ <azureneb@gmail.com>
2021-07-14 00:31:15 -0600

    Cambios en la barra de navegación

--- Frontend/XelaByte/src/app/home/home.component.ts
+++ Frontend/XelaByte/src/app/home/home.component.ts
@@ -15,15 +15,22 @@ export class HomeComponent implements OnInit, AfterViewInit
 {
   private FormsPairs = function()
   {
-    let FormButtons = Array.from(document.getElementsByName("FormButton"));
+    //let FormButtons = Array.from(document.getElementsByName("FormButton"));
+    let FormButtons = 
+    [ 
+      Array.from(document.getElementsByName("ContactFormButton")),
+      Array.from(document.getElementsByName("TalentFormButton")),
+      Array.from(document.getElementsByName("MakeAppointmentFormButton"))
+    ];
+
     let FormCloseButtons = Array.from(document.getElementsByName("FormCloseButton"));
     let Forms = Array.from(document.getElementsByName("Form"));
     let res = [];
     for(let x = 0; x < Forms.length; x++)
     {
       res.push(
-      {
-        Button: FormButtons[x],
+      { 
+        Buttons: FormButtons[x],
         CloseButton: FormCloseButtons[x],
         Form: Forms[x]
       });
@@ -92,7 +99,7 @@ export class HomeComponent implements OnInit, AfterViewInit
 
   SetServicesFormAnimation(): void
   {
-    console.log("AfterViewInit");
+    //console.log("AfterViewInit");
     //FORMULARIO DE SERVICIOS
     let ServiceFormButtons = Array.from(document.getElementsByName("ServiceFormButton"));
     let ServiceFormCloseButton = document.getElementsByName("ServiceFormCloseButton")[0] as HTMLElement;
@@ -134,19 +141,22 @@ export class HomeComponent implements OnInit, AfterViewInit
     for(let item of this.FormsPairs())
     {
       /*ABRIENDO EL FORM*/
-      //CUANDO HACE CLICK AL BOTÓN PARA MOSTRAR EL FORMULARIO
-      item.Button.onclick = () => 
-      { 
-        item.Form.classList.toggle("d-block");
-        setTimeout(() => 
-        {
-          if(this.IsMobile()) document.body.classList.toggle("ModalActive-m");
-          else document.body.classList.toggle("ModalActive");
-          item.Form.classList.toggle("Show"); item.Form.children[0].children[0].classList.toggle("Show-content");
-        }, 2)
-      };
-      /*----------------*/
-
+      for(let formButton of item.Buttons)
+      {
+        //CUANDO HACE CLICK AL BOTÓN PARA MOSTRAR EL FORMULARIO
+        formButton.onclick = () => 
+        { 
+          item.Form.classList.toggle("d-block");
+          setTimeout(() => 
+          {
+            if(this.IsMobile()) document.body.classList.toggle("ModalActive-m");
+            else document.body.classList.toggle("ModalActive");
+            item.Form.classList.toggle("Show"); item.Form.children[0].children[0].classList.toggle("Show-content");
+          }, 2)
+        };
+        /*----------------*/
+      }
+      
       /*CERRANDO EL FORM*/
       let isMobile = this.IsMobile();
       let OnExitForm = function()
@@ -169,12 +179,6 @@ export class HomeComponent implements OnInit, AfterViewInit
       item.CloseButton.onclick = function() { OnExitForm(); };
       /*----------------*/
     }
-  }
-
-  goToServices()
-  {
-    if(this.IsMobile()) { window.scrollTo(0, 2116); }
-    else { window.scrollTo(0, 2661); }
   }
   
   IsMobile(): boolean { return (/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Windows Phone/i.test(navigator.userAgent)) ? true : false; }
